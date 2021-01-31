2b19401ce68942ace08bdede35200028fd4ba66b
georgiybykov <zhora.bykov@gmail.com>
2021-01-31 04:48:34 +0300

    Add `Rack::ContentType` and the http statuses

--- app.rb
+++ app.rb
@@ -1,26 +1,24 @@
 # frozen_string_literal: true
 
 class App
+  HTTP_STATUS = { ok: 200, bad_request: 400 }.freeze
+
   def call(env)
     @request = Rack::Request.new(env)
     @formatted_params = ParamsFormatter.new(request.params)
 
-    [status, headers, body]
+    [status, {}, body]
   end
 
   private
 
   attr_reader :request, :formatted_params
 
-  def status
-    formatted_params.errors.empty? ? 200 : 400
-  end
-
-  def headers
-    { 'Content-Type' => 'text/plain' }
-  end
-
   def body
     formatted_params.body
+  end
+
+  def status
+    formatted_params.errors.empty? ? HTTP_STATUS[:ok] : HTTP_STATUS[:bad_request]
   end
 end
--- config.ru
+++ config.ru
@@ -7,5 +7,7 @@ require_relative 'middleware/app_logger'
 require_relative 'app'
 
 use Dispathcer
+# run Rack::URLMap.new "/time" => AppLogger
 use AppLogger, logdev: File.expand_path('log/app.log', __dir__)
+use Rack::ContentType, 'text/plain'
 run App.new
